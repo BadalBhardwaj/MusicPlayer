c89eb4cabc37343dfd663554dcd37ffde2bb534d
Badal Bhardwaj <badalbhardwaj111@gmail.com>
2023-05-17 21:22:04 +0530

    MusicPlayer

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+
+let allMusic = [
+    {
+      name: "Baka",
+      artist: "luffy",
+      img: "baka",
+      src: "baka"
+    },
+    {
+      name: "Top G",
+      artist: "Andrew Tate",
+      img: "top g",
+      src: "top G"
+    },
+    {
+      name: "Sunn Naa",
+      artist: "Bali",
+      img: "sunn naa",
+      src: "Sunn naa"
+    },
+    {
+      name: "Mujhe nhi pta hai",
+      artist: "King",
+      img: "Muhje nhi pta hai",
+      src: "Muhje nhi pta hai"
+    },
+    {
+      name: "Waka Waka",
+      artist: "Sakira",
+      img: "Waka Waka",
+      src: "Waka Waka"
+    },
+    {
+      name: "Memories",
+      artist: "Maroon",
+      img: "Memories",
+      src: "Memories"
+    },
+  ];
